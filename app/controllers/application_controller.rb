579d6267aceef9fc8eb6895430b4fc99054b0103
Ian Marshall <imarshall14@gmail.com>
2021-01-28 15:47:02 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,40 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.new(title: params[:title], content: params[:content])
+    erb :show
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(:id)
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article.title = params[:title]
+    @article.content = parms[:content]
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    article = Article.find_by(:id)
+    article.delete
   end
 end
